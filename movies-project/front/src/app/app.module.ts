12eda0d90d0d45ca81beba0fb4fe3e8bbf9aded6
ahmadyassinkhoja <ahmad.mohammad.khoja@gmail.com>
2018-11-16 10:51:55 +0200

    added animation

--- movies-project/front/src/app/app.module.ts
+++ movies-project/front/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -28,7 +29,7 @@ import { FileSelectDirective } from 'ng2-file-upload';
     HttpClientModule,
     FormsModule,
     AppRoutingModule,
-    // AngularFileUploaderModule
+    BrowserAnimationsModule
   ],
   providers: [MoviesService],
   bootstrap: [AppComponent]
--- movies-project/front/src/app/movies/movies.component.ts
+++ movies-project/front/src/app/movies/movies.component.ts
@@ -4,12 +4,34 @@ import { MoviesService } from './movies.service'
 
 import {  FileUploader, FileSelectDirective } from 'ng2-file-upload/ng2-file-upload';
 
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+
+
 const URL = 'http://localhost:3000/addPhoto';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
-  styleUrls: ['./movies.component.css']
+  styleUrls: ['./movies.component.css'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+        query(':enter', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true}),
+          query(':leave', stagger('300ms', [
+            animate('1s ease-in', keyframes([
+              style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+              style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+              style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+            ]))]), {optional: true})
+      ]),
+    ]),
+  ]
 })
 export class MoviesComponent implements OnInit, DoCheck {
   movies
